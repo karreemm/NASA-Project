c573d7fb5e2d1fa5fbd5fe7d0cdf3ecc63c20af9
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2024-10-04 22:33:10 +0300

    finish planets branch

--- app/planets/page.jsx
+++ app/planets/page.jsx
@@ -44,6 +44,21 @@ const dummyData = [
     "image": "https://via.placeholder.com/150",
   },
   {
+    "pl_name": "AU Mic c",
+    "hostname": "AU Mic",
+    "image": "https://via.placeholder.com/150",
+  },
+  {
+    "pl_name": "BD+20 594 b",
+    "hostname": "BD+20 594",
+    "image": "https://via.placeholder.com/150",
+  },
+  {
+    "pl_name": "BD+20 594 b",
+    "hostname": "BD+20 594",
+    "image": "https://via.placeholder.com/150",
+  },
+  {
     "pl_name": "BD-14 3065 b",
     "hostname": "BD-14 3065 A",
     "image": "https://via.placeholder.com/150",
@@ -64,8 +79,8 @@ const Planets = () => {
   }
 
   return (
-    
-    <div className='w-full h-[1500px] relative bg-black'>
+
+    <div className='w-full h-[2100px] bg-black'>
 
       <Canvas
         camera={{ position: [0, 0, 5] }}
@@ -86,22 +101,26 @@ const Planets = () => {
       </Canvas>
 
       {/* Main content with flex layout */}
-      <div className=' absolute top-0 mx-10  flex flex-col justify-center '>
-        
+      <div className=' absolute top-0 mx-10  flex flex-col justify-center px-10 pt-10'>
+
         <div className='flex justify-between w-full  my-5'>
-          
+
           {/* Planets Container */}
           <div className='w-[70%] text-white m-5 rounded-md flex flex-col'>
-            <h1 className='text-3xl sm:text-5xl font-bold p-5 mt-5'>
+            <h1 className='text-3xl  sm:text-5xl font-bold p-5 mt-5'>
               Explore Planets
             </h1>
             <div className='w-[200px] border-b-4 border-gray-500 mb-10 ml-5'></div>
             <div className='text-xl sm:text-xl font-bold mb-5 p-5'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
+              Exploring exoplanets—worlds orbiting stars beyond our solar system—represents an exciting frontier in astronomy.
+              With advanced detection techniques like the transit method and radial velocity, scientists can uncover diverse planetary
+              characteristics, from gas giants to rocky worlds in the habitable zone. Studying these exoplanets helps us understand their atmospheres,
+              compositions,
+              and potential for life, ultimately enriching our knowledge of the universe and our place within it.
             </div>
 
             {/* Displaying recommended planets as cards in a grid */}
-            <div className='grid grid-cols-3 gap-10 p-10'>
+            <div className='grid grid-cols-3 gap-10 py-10 px-5'>
               {filteredResults.map((planet) => (
                 <Planet key={planet.pl_name} planet={planet} />
               ))}
@@ -109,7 +128,7 @@ const Planets = () => {
           </div>
 
           {/* Search Container */}
-          <div className='w-[25%] text-white m-5 mt-20 p-5 rounded-md flex flex-col h-[500px] border border-2 '>
+          <div className='w-[25%] text-white m-5 mt-20 p-5 rounded-lg flex flex-col h-[500px] border border-2 '>
             <Search dummyData={dummyData} setQuery={setQuery} />
           </div>
         </div>
